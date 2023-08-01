2044f598f4f1d34bc496afb6bf84345b69867f7c
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-08-01 12:18:53 -0700

    Change punctuation

--- open_cv/render/render.cpp
+++ open_cv/render/render.cpp
@@ -7,38 +7,38 @@ cv::Mat caboodle(std::string path, cv::Mat img, bool saving) {
         cv::cvtColor(image, image, cv::COLOR_RGBA2RGB);
     }
 
-    std::cout << "begining fully automated hedcut detection" << std::endl;
+    std::cout << "Beginning fully automated hedcut detection." << std::endl;
     // step 1: extract the edges of the image
     cv::Mat edges = extractEdges(path, image, 300, false);
     if (edges.type() != 0) {
         edges.convertTo(edges, 0);
     }
 
-    std::cout << "extracted edges from image" << std::endl;
+    std::cout << "Extracted edges from image." << std::endl;
 
     // step 2: extract the isophotes of the image
     cv::Mat isophotes = extractIsophotes(path, image, 10, 1, false);
     if (isophotes.type() != 0) {
         isophotes.convertTo(isophotes, 0);
     }
-    std::cout << "extracted isophotes from image" << std::endl;
+    std::cout << "Extracted isophotes from image." << std::endl;
 
     // step 3: offset map
     cv::Mat map = fullMap(path, edges, path, isophotes, 6.0, true, false);
-    std::cout << "extracted offset map from image" << std::endl;
+    std::cout << "Extracted offset map from image." << std::endl;
 
     // step 4: generate initial dot placement
     cv::Mat seeds = placeSeeds(path, map, 6.0, false);
-    std::cout << "finalized initial dot placement for image" << std::endl;
+    std::cout << "Finalized initial dot placement for image." << std::endl;
 
     // step 5: generate final dot placement
-    std::cout << "begining dot adjustment, THIS PROCESS IS SLOW" << std::endl;
+    std::cout << "Beginning dot adjustment, THIS PROCESS IS SLOW." << std::endl;
     cv::Mat adjusted = dots(path, map, path, seeds, 6.0, false);
-    std::cout << "finalized dot placement for image" << std::endl;
+    std::cout << "Finalized dot placement for image." << std::endl;
 
     // step 6: place circles!
     cv::Mat rendered = placeDots(path, adjusted, path, image, 20, true);
-    std::cout << "sized dots" << std::endl;
+    std::cout << "Sized dots, done." << std::endl;
     save(rendered, path, "-rendered");
 
     return rendered;
